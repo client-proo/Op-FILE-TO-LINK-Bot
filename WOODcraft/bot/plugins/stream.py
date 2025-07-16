9d9a5b38e7aeb7631ecf52fb285796938260c29e
client-proo <client.proo2024@gmail.com>
2025-07-16 21:48:50 +0330

    Update stream.py

--- WOODcraft/bot/plugins/stream.py
+++ WOODcraft/bot/plugins/stream.py
@@ -36,15 +36,15 @@ async def private_receive_handler(c: Client, m: Message):
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
        
-        msg_text ="""<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>🦋 ᴅᴏᴡɴʟᴏᴀᴅ 🦋 :</b> <i>{}</i>\n\n<b>❣️ ᴡᴀᴛᴄʜ ❣️ :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : Link Won't Expire Till I Delete...</b>"""
+        msg_text ="""<i><u>لـیـنـک فـایـل شـمـا سـاخـتـه شـد ✅ !</u></i>\n\n<b>📂 نام فایل:</b> <i>{}</i>\n\n<b>📦 حجم فایل:</b> <i>{}</i>\n\n<b>📥 لینک دانلود:</b> <i>{}</i>\n\n<b>🖥️ پخش آنلاین:</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : Link Won't Expire Till I Delete...</b>"""
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
             text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⎚ 𝐒𝐭𝐫𝐞𝐚𝐦 ⎚", url=stream_link), #Stream Link
-                                                InlineKeyboardButton('⎙ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ⎙', url=online_link)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥️ پخش آنلاین", url=stream_link), #Stream Link
+                                                InlineKeyboardButton('📥 دانلود', url=online_link)]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
@@ -71,8 +71,8 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("⎚ 𝐒𝐭𝐫𝐞𝐚𝐦 ⎚", url=stream_link),
-                     InlineKeyboardButton('⎙ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ⎙', url=online_link)] 
+                    [InlineKeyboardButton("🖥️ پخش آنلاین", url=stream_link),
+                     InlineKeyboardButton('📥 دانلود', url=online_link)] 
                 ]
             )
         )
